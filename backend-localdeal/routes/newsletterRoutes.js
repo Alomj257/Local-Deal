5a6fb7ed3e6edb7978f9f1d44a32e8f75c4e0c26
Jahangir Alom <alomj257@gmail.com>
2024-02-22 02:25:55 +0530

    Update newsletterRoutes.js

--- backend-localdeal/routes/newsletterRoutes.js
+++ backend-localdeal/routes/newsletterRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const newsletterController = require('../controllers/NewsletterController');
+const newsletterController = require('../controllers/newsletterController');
 
 // Subscribe to newsletter route
 router.post('/subscribe', newsletterController.subscribeToNewsletter);
